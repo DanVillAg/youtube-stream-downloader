80b4bb40040fb118c3de67dd4b1d85ea4accc7c5
DanVillAg <danielvillegasaguilar@gmial.com>
2020-10-01 18:59:45 -0500

    tinkering with gcpp

--- simple-download.py
+++ simple-download.py
@@ -4,7 +4,7 @@ import time
 import subprocess
 
 ### CHANGE THE URL HERE ####
-URL = 'https://www.youtube.com/watch?v=1EiC9bvVGnk'
+URL = 'https://www.youtube.com/watch?v=XJfpbcIyETo'
 quality = 'best' # can choose from 720p, 480p, 240p 
 
 # define Job
@@ -12,9 +12,14 @@ def job_function():
     now = datetime.datetime.now()
     outputfile = str(now.year)+'-'+str(now.month)+'-'+str(now.day)+'-'+str(now.hour)+str(now.minute)+'.ts'
     
-    command='streamlink ' + URL + ' '+ quality + ' --hls-start-offset 01:00:00 --hls-duration 01:00:00 -o ' + outputfile
+    command='streamlink ' + URL + ' '+ quality + ' --hls-start-offset 00:01:00 --hls-duration 00:01:00 -o ' + outputfile
     subprocess.call(command, shell=True)
+    command2 = 'gsutil cp ' + outputfile + ' gs://transfer-bucket-cc/'+str(now.year)+'-'+str(now.month)+'-'+str(now.day)+'/'
+    subprocess.call(command2, shell=True)
     time.sleep(20)
 
 # Schedules job_function
 job_function()
+
+
+#gsutil cp raulms-object-one.txt gs://transfer-bucket-cc
